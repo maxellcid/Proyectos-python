89cdb396952427820388643dccab354cd5369bef
Arnulfo Díaz <maxellcid@gmail.com>
2023-09-19 12:06:36 -0400

    ingreso de datos y  muestra del texto en pantalla

--- 01-madlibs/madlibs.py
+++ 01-madlibs/madlibs.py
@@ -5,14 +5,26 @@
 organizacion = "freeCodeCamp"
 
 # codigo mas limpio
-print("Aprende a programr con " + organizacion + ".")
+print("Aprende a programar con " + organizacion + ".")
 
 # codigo utiliza un metodo llamado .format
-print("Aprende a programr con {}.".format(organizacion))
+print("Aprende a programar con {}.".format(organizacion))
 
 # codigo no tan funcional
-print("Aprende a programr con ", organizacion, ".")
+print("Aprende a programar con ", organizacion, ".")
 
 
 # f.string
-print(f"Aprende a programr con {organizacion}.")
+print(f"Aprende a programar con {organizacion}.")
+
+# Mad Libs
+
+adj = input("Adjetivo: ")
+verbo1 = input("verbo: ")
+verbo2 = input("verbo: ")
+sustantivo_plural = input("Sustantivo plural: ")
+
+
+madlib = f"¡Programar es tan {adj}! Siempre me emociona porque me encanta {verbo1} problemas.¡aprende a {verbo2} con Freecodecamp y alcanza tus {sustantivo_plural}!"
+
+print(madlib)
