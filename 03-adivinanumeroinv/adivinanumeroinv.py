2103e0bdb3d6a68cc4fa384319c2471bbd87266a
Arnulfo Díaz <maxellcid@gmail.com>
2023-09-20 13:18:23 -0400

    3er proyecto finalizado

--- 03-adivinanumeroinv/adivinanumeroinv.py
+++ 03-adivinanumeroinv/adivinanumeroinv.py
@@ -1,3 +1,36 @@
 # escribir un programa que adivine un numero sugerido por el jugador
+
+import random
+
+
 def adivina_el_numero_computadora(x):
-    print("¡Bienvenido(a) al juego!")
+
+    print("¡Bienvenido(a) al juego! ")
+    print(
+        f"selecciona un numero entre 1 y {x} para  adivinarlo.")
+
+    limite_inferior = 1
+    limite_superior = x
+    respuesta = ""
+
+    while respuesta != "c":
+        # Generar una prediccion
+        if limite_inferior != limite_superior:
+            prediccion = random.randint(limite_inferior, limite_superior)
+        else:
+            prediccion = limite_inferior
+            # tambien podria ser el limite superior
+            # obtener respuestas del ususario
+        respuesta = input(
+            f"mi prediccion es {prediccion}. Si es muy alta,ingresa (A). Si es muy baja, Ingresa (B). si es correcta, ingresa (C)").lower()
+
+        if respuesta == "a":
+            limite_superior = prediccion - 1
+        elif respuesta == "b":
+            limite_inferior = prediccion + 1
+
+    print(
+        f"Felicitaciones la computadora adivino tu numero correctamente: {prediccion}")
+
+
+adivina_el_numero_computadora(100)
